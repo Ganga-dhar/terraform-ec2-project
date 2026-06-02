c7b50a21970d6b650cb632faab2fba22777101e8
advita.vedanta01@gmail.com <advita.vedanta01@gmail.com>
2026-06-02 17:37:44 +0530

    Initial Terraform project

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-demo1111"
+    key    = "terraform/terraform.state"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
